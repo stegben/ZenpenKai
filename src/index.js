8d0229dfaf91e0d495005a3e64acb277e2946466
cph <stegben.benjamin@gmail.com>
2016-05-07 21:56:24 +0800

    little try on css loader

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import './index.css';
+
 class ZenPenKai extends React.Component {
   render() {
     return (
-      <h1>Hello Zen Pen</h1>
+      <h1 className="test">Hello Zen Pen</h1>
     );
   }
 }
