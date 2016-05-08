e29517280e2bbc85acd968a79ff9088f9927dbeb
cph <stegben.benjamin@gmail.com>
2016-05-08 16:26:41 +0800

    checkpoint 5/8

---
+++ src/Components/title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextField from 'material-ui/lib/text-field';
+
+class Title extends React.Component {
+  render() {
+
+  }
+}
+
+export default Title;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import Title from './Components/title';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 import './index.css';
 
 class ZenPenKai extends React.Component {
+  constructor() {
+
+  }
   render() {
     return (
-      <h1 className="test">Hi Zen Pen</h1>
+      <div>
+        <Title />
+        <h1 className="test">Hi Zen Pen</h1>
+      </div>
     );
   }
 }
