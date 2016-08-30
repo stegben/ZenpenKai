9f39a46b91a8033682037e438ec499a1fa00ba12
cph <stegben.benjamin@gmail.com>
2016-08-30 17:20:31 +0800

    make test pass, change folder name

--- src/index.js
+++ src/index.js
@@ -2,20 +2,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import Title from './Components/title';
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
-// Needed for onTouchTap
-// Check this repo:
-// https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin();
+import Title from './components/title';
 
 import './index.css';
 
 class ZenPenKai extends React.Component {
-  constructor() {
-
-  }
   render() {
     return (
       <div>
